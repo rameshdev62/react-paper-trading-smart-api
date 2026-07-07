eae75d23e0f9fc1f4f4392335683299a2a730b56
Ramesh dev062 <ramesh.dev062@gmail.com>
2026-07-07 23:17:23 +0530

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["smartapi-javascript"],
+  serverExternalPackages: ["smartapi-javascript", "@prisma/client", "better-sqlite3"],
 };
 
 export default nextConfig;
