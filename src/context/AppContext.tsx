07d9daea16673df37de0817624e24707df39a5bb
Ramesh dev062 <ramesh.dev062@gmail.com>
2026-07-09 00:34:15 +0530

    fix supabase key error

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -4,8 +4,8 @@ import React, { createContext, useContext, useState, useEffect, useRef } from "r
 import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
